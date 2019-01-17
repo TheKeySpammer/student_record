c7f65e4e48626def2a5ea158bb9facb6f8b8d972
Aamir <gt_ams@yahoo.in>
2019-01-17 19:39:52 +0530

    Update display page

--- display.php
+++ display.php
@@ -2,7 +2,7 @@
 require("partials/header.html");
 
 $db = new mysqli('localhost', 'ursa', 'ulsafar', 'dbLab');
-if(mysqli_connect_errno){
+if(mysqli_connect_errno()){
     ?>
     <div class="text-center"> 
     <h2 class="alert alert-danger">
@@ -11,20 +11,50 @@ if(mysqli_connect_errno){
     <a href="index.html" class="btn btn-primary">Go Back</a>
     </div>
     <?php
-}
+}else{
 ?>
 <div class="jumbotron text-center">
-    <h1 class="display-2">Student Records</h1>
-    <div class="row ">
+    <h3 class="display-2 mb-3">Student Records</h3>
+    <div class="row">
     <div class="col">
-        
+        <a href="#" class="btn btn-primary btn-lg rounded-pill ml-3"><i class="fas fa-plus-circle mr-2"></i>Add Student</a>
     </div>
-    <div class = "col">
+    <div class = "col d-flex justify-content-start">
+        <a href="#" class="btn btn-success rounded-pill btn-lg mr-3"><i class="fas fa-search mr-2"></i>Search</a>
     </div>
     </div>
 </div>
 
+<h2 class="text-center">List of archived students</h2>
+<div class="row justify-content-center">
+    <div class="col-8">
+        <ul class="list-group">
+        
+            <?php
+            $query = "select Name from std_record";
+            $stmt = $db->prepare($query);
+            $stmt->execute();
+            $stmt->bind_result($std_name);
+            while($stmt->fetch()){
+            ?>
+            <li class = "list-group-item">
+                <div class="row">
+                    <div class="col student_record_disp">
+                        <? echo $std_name ?>
+                    </div>
+                    <div class="col student_record_del"></div>
+                </div>
+            </li>
+            <?php } ?>
+        
+        </ul>
+    </div>
+</div>
 
 <?php
+}
+?>
+<?php
+$db->close();
 require("partials/footer.html");
 ?>
